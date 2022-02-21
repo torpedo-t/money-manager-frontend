ffa51f8e1b3333b40cd782e83422c198b84eb628
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2022-02-21 15:41:28 -0600

    installed redux and others in package.json

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 class App extends React.Component {
   
-  componentDidMount() {
-    fetch('http://localhost:3000/api/v1/bank_accounts')
-    .then(response => response.json())
-    .then(data => console.log(data))
-  }
+  // componentDidMount() {
+  //   fetch('http://localhost:3000/api/v1/bank_accounts')
+  //   .then(response => response.json())
+  //   .then(data => console.log(data))
+  // }
 
   render() {
     return (
